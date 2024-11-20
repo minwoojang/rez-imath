749c853780f46c65c8bb8a063e981df57c2c7ae3
minwoojang <q860514@naver.com>
2024-11-20 12:53:08 +0900

    add 3.1.12

---
+++ 3.1.12/package.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from rez.utils.lint_helper import env, building
+
+
+name = "imath"
+
+version = "3.1.12"
+
+description = \
+    """
+    Imath
+    """
+
+build_requires = [
+    # 'cmake-3',
+#    "ilmbase"
+]
+
+requires = [
+    # 'boost-1.61',
+    #'numpy'
+]
+
+variants = [["platform-linux", "arch-x86_64"]]
+
+uuid = "repository.openexr"
+
+
+def commands():
+    env.LD_LIBRARY_PATH.prepend("{root}/lib64")
+    env.PKG_CONFIG_PATH.prepend("{root}/lib64/pkgconfig")
+    env.INCLUDE_PATH.prepend("{root}/include")
+
+    if building:
+        env.CMAKE_MODULE_PATH.append("{root}/cmake")
+
+uuid = "repository.openexr"
